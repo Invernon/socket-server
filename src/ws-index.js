3eef5ec61ca538a8d7861d783bfa814e2e9a560a
Jose Invernon <j.inver93@gmail.com>
2020-09-13 22:09:35 -0400

    Server Working

--- src/ws-index.js
+++ src/ws-index.js
@@ -3,6 +3,8 @@ const https = require('https');
 const WebSocket = require('ws');
 // uWebSockets.js is binary by default
 const { StringDecoder } = require('string_decoder');
+const socketIoStream = require('socket.io-stream');
+const { client } = require('websocket');
 const decoder = new TextDecoder('utf8');
 const port = 5000;
 
@@ -28,9 +30,10 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
 
-
+    console.log('new Connection');
     ws.id = Math.random() * 1000;
     ws.username = createName(getRandomInt());
+    // ws.isMaster = false;
 
     // Clients on server
     CLIENTS.push(ws);
@@ -59,8 +62,25 @@ wss.on('connection', function connection(ws) {
 
         console.log(message);
         // decode message from client
-        // let clientMsg = JSON.parse(decoder.decode(message));
-        let serverMsg = {};
+        // let clientMsg = JSON.parse(message);
+        // let serverMsg = {};
+
+        // if (clientMsg.type && clientMsg.type === 'REQUEST_MASTER') {
+        //     wss.clients.forEach(function each(client) {
+        //         if (client !== ws && client.readyState === WebSocket.OPEN) {
+        //             client.send(message);
+        //         }
+        //     });
+
+        //     CLIENTS.find(e => e.name === clientMsg.body).send({
+        //         type: 'YOU_ARE_MASTER',
+        //         body: {
+        //             master: true
+        //         } 
+        //     });
+        // } else {
+
+        // }
 
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -68,84 +88,14 @@ wss.on('connection', function connection(ws) {
             }
         });
 
+
         //   console.log('received: %s', message);
     });
 });
 
 server.listen(port, (listenSocket) => {
-    listenSocket ?
-        console.log(`Listening to port ${port}`) :
-        console.log(`Failed to listen to port ${port}`);
+    console.log(`Listening to port ${port}`)
 })
-
-// const app = uWS.SSLApp(settings).ws('/*', {
-//     compression: 0,
-//     maxPayloadLength: 16 * 1024 * 1024,
-//     idleTimeout: 0,
-
-//     open: (ws, req) => {
-//         ws.id = Math.random() * 1000;
-//         ws.username = createName(getRandomInt());
-//         // subscribe to topics
-//         ws.subscribe(MESSAGE_ENUM.CLIENT_CONNECTED);
-//         ws.subscribe(MESSAGE_ENUM.CLIENT_DISCONNECTED);
-//         ws.subscribe(MESSAGE_ENUM.CLIENT_MESSAGE);
-
-//         // Clients on server
-//         CLIENTS.push(ws);
-//         // indicate message type so the client can filter with a switch statement later on
-//         let selfMsg = {
-//             type: MESSAGE_ENUM.SELF_CONNECTED,
-//             body: {
-//                 id: ws.id,
-//                 name: ws.username
-//             }
-//         }
-//         let pubMsg = {
-//             type: MESSAGE_ENUM.CLIENT_CONNECTED,
-//             body: {
-//                 id: ws.id,
-//                 name: ws.username
-//             }
-//         }
-//         // send to connecting socket only
-//         ws.send(JSON.stringify(selfMsg));
-
-//         // send to *all* subscribed sockets
-//         // app.publish(MESSAGE_ENUM.CLIENT_CONNECTED, pubMsg)
-
-//         // send to connecting socket
-//         // socket.send(JSON.stringify(msg));
-//     },
-//     message: (ws, message, isBinary) => {
-//         /* In this simplified example we only have drawing commands */
-//         console.log('Message', message, false);
-//         // app.publish("drawing/canvas1", message, true);
-//         // decode message from client
-//         let clientMsg = JSON.parse(decoder.decode(message));
-//         let serverMsg = {};
-
-//         switch (clientMsg.type) {
-//             case MESSAGE_ENUM.CLIENT_MESSAGE:
-//                 serverMsg = {
-//                     type: MESSAGE_ENUM.CLIENT_MESSAGE,
-//                     sender: ws.username,
-//                     body: clientMsg.body
-//                 };
-
-//                 app.publish(MESSAGE_ENUM.CLIENT_MESSAGE, JSON.stringify(serverMsg));
-//                 break;
-//             default:
-//                 console.log("Unknown message type.");
-//         }
-//     },
-// });
-// // finally listen using the app on port 3000
-// app.listen(port, (listenSocket) => {
-//     listenSocket ?
-//         console.log(`Listening to port ${port}`) :
-//         console.log(`Failed to listen to port ${port}`);
-// })
 
 // functions to help us generate random usernames
 function getRandomInt() {
